b16ff230780cc7a405cdfe3cd4586e2b83010cef
Sina Hosseini <sina.hosseini.gst@gmail.com>
2023-08-15 14:13:32 +0430

    Add  to Git

--- src/App.vue
+++ src/App.vue
@@ -129,9 +129,7 @@
         This website is constantly being updated, and this means that every time you enter, you're likely to see more CONTENT! So, stay tuned!
       </p>
       <p class="my-2 py-2 text-white border-y-8 border-double border-c-gray-8">
-        <span class="text-c-blue-3">UPDATE</span>: <span class="text-c-blue-2">Business Fax</span> <span class="text-red-500">Translated</span> - <span class="text-purple-600">RE3N</span>
-        <br>
-        <span class="text-c-blue-3">UPDATE</span>: <span class="text-c-blue-2">Dario's Memo</span> <span class="text-red-500">Translated</span> - <span class="text-purple-600">RE3N</span>
+        <span class="text-c-blue-3">UPDATE</span>: <span class="text-c-blue-2">Operation Instruction</span> <span class="text-red-500">Translated</span> - <span class="text-purple-600">RE3N</span>
       </p>
       <p class="text-justify text-orange-300 uppercase">
         Many thanks to <a target="_blank" class="underline underline-offset-2 hover:font-bold text-orange-500" href="https://www.evilresource.com/">Evil Resource</a>, and <a target="_blank" class="underline underline-offset-2 hover:font-bold text-orange-500" href="https://residentevil.fandom.com/wiki/Resident_Evil_Wiki">Resident Evil Wiki</a>, for helping me shape the project!
