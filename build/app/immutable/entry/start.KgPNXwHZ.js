3de22cef9d809b318c4d743af24f9b3abc61eb53
Sina Hosseini <sina.hosseini.gst@gmail.com>
2024-04-07 18:07:47 +0430

    Add v1.1 to Git

---
+++ build/app/immutable/entry/start.KgPNXwHZ.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.C9znJfGi.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		appDir: 'app',
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
@@ -12,10 +15,13 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
+			fallback: '404.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	},
   preprocess: vitePreprocess()
 };
