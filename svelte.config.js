802a51524e0e7f7c5ea45655da694b65380d7f73
Sina-Hosseini-GST <sina.hosseini.gst@gmail.com>
2024-11-30 17:34:13 +0330

    Set up svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	},
   preprocess: vitePreprocess()
 };
